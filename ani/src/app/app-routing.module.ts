279d811a32329c45e41fbdffabb86d17d1662bd4
Bosi <dexing30100@gmail.com>
2019-09-23 14:11:15 +1000

    added routelink

--- ani/src/app/app-routing.module.ts
+++ ani/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ResultComponent } from './components/result/result.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ResultComponent },
+{ path: 'about', component: AboutComponent }, { path: 'profile', component: ProfileComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
