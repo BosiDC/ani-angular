f8f1ffa9216a3254cf96a469aaad4cfbfa3f33c7
Bosi <dexing30100@gmail.com>
2019-09-23 15:42:33 +1000

    basic top 5 and basic search ui

--- ani/src/app/models/Anime.ts
+++ ani/src/app/models/Anime.ts
@@ -1,7 +1,13 @@
 export class Anime {
+    mal_id: number;
     rank: number;
     title: string;
+    url: string;
+    image_url: string;
     type: string;
     episodes: number;
+    start_date: string;
+    end_date: string;
+    members: number;
     score: number;
 }
