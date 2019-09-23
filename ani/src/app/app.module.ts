125f0e52bd56ab23ffcbaca66638bec06de75b8e
Bosi <dexing30100@gmail.com>
2019-09-23 20:54:03 +1000

    added angular universal + app.yaml for app engine

--- ani/src/app/app.module.ts
+++ ani/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +24,7 @@ import { ResultItemComponent } from './components/result-item/result-item.compon
     ResultItemComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule,
     FormsModule
