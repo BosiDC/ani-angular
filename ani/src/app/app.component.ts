3d564c4c155ecaded00f05ad6a58d6f69902c7c2
Bosi <dexing30100@gmail.com>
2019-09-27 13:50:53 +1000

    no angular universal

--- ani/src/app/app.component.ts
+++ ani/src/app/app.component.ts
@@ -1,28 +1,12 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { templateJitUrl } from '@angular/compiler';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { templateJitUrl } from "@angular/compiler";
 
 @Component({
-  selector: 'app-root',
-  //templateUrl: './app.component.html',
- // styleUrls: ['./app.component.css'],
-
-  //nebular template:
-  template: `
-
-  <nb-layout>
-  <nb-layout-header fixed>Anii</nb-layout-header>
-
-  <nb-sidebar>Anime Search</nb-sidebar>
-
-  <nb-layout-column>
-    Page Content <button nbButton>Hello World</button>
-  </nb-layout-column>
-</nb-layout>
-  
-  `
-
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ani';
+  title = "ani";
 }
