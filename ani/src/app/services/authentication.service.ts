fdfe1d48d18f15a877112e1708753830a935978c
Bosi <dexing30100@gmail.com>
2019-10-04 22:36:15 +1000

    button changes to sign out if user is logged on

--- ani/src/app/services/authentication.service.ts
+++ ani/src/app/services/authentication.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { auth } from "firebase/app";
+import { AngularFireAuth } from "@angular/fire/auth";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from "@angular/fire/firestore";
+
+import { Observable, of } from "rxjs";
+import { switchMap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthenticationService {
-  constructor() {}
+  users$: Observable<any>;
+  constructor(
+    private router: Router,
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore
+  ) {
+    this.users$ = this.afAuth.authState;
+  }
+
+  signOut() {
+    return this.afAuth.auth.signOut().then(() => {
+      this.router.navigate(["/"]);
+    });
+  }
+
+  private update;
 }
