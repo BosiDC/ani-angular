8f548efe4e8820dd9942f09a72e1c10301a61d19
Bosi <dexing30100@gmail.com>
2019-10-04 18:09:18 +1000

    edited login and header component

--- ani/src/app/pages/login/login.component.ts
+++ ani/src/app/pages/login/login.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthProvider } from "ngx-auth-firebaseui";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  providers = AuthProvider;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
