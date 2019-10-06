a98387cac31b68d7c9d7ef04e638c99414c3984c
Bosi <dexing30100@gmail.com>
2019-10-07 00:05:31 +1100

    edited to allow for --build

--- ani/src/environments/environment.prod.ts
+++ ani/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD-WnXsJ6CAR6B4BbpxIXfmBeRE5cEiddc",
+    authDomain: "anii-420.firebaseapp.com",
+    databaseURL: "https://anii-420.firebaseio.com",
+    projectId: "anii-420",
+    storageBucket: "anii-420.appspot.com",
+    messagingSenderId: "10244473983",
+    appId: "1:10244473983:web:842f9903f631eb40276b4a",
+    measurementId: "G-JMJKXCWJK0"
+  }
 };
