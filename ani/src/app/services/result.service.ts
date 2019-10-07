2055616bb06c1eeb588661961dea0c90608759f9
Bosi <dexing30100@gmail.com>
2019-10-07 23:24:23 +1100

    added loading spinners

--- ani/src/app/services/result.service.ts
+++ ani/src/app/services/result.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from "@angular/core";
 import { Top, Search, Current } from "../models/Anime";
 import { Observable } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { BehaviorSubject } from "rxjs";
+import { ɵNAMESPACE_URIS } from "@angular/platform-browser";
 
 @Injectable({
   providedIn: "root"
 })
 export class ResultService {
   topURL: string = "https://api.jikan.moe/v3/top/anime/1/";
-  searchURL: string = "https://api.jikan.moe/v3/search/anime?q=";
+  searchURL: string = "https://api.jikan.moe/v3/search/anime?";
   currentURL: string = "https://api.jikan.moe/v3/season/2019/fall";
+  URL: string = "";
 
   constructor(private http: HttpClient) {}
 
@@ -31,20 +33,22 @@ export class ResultService {
     rating: string,
     genre: string
   ): Observable<Search[]> {
-    return this.http.get<Search[]>(
-      this.searchURL +
-        name +
-        "&order_by=" +
-        order +
-        "&type=" +
-        type +
-        "&status=" +
-        status +
-        "&rated=" +
-        rating +
-        "&genre=" +
-        genre
-    );
+    let httpParams = new HttpParams();
+    if (name == "") {
+      httpParams.set("q", "");
+    } else {
+      httpParams.set("q", name);
+    }
+    if (order == "") {
+      httpParams.set("order_by", "");
+    } else {
+      httpParams.set("order_by", order);
+    }
+    if (type == "") {
+      httpParams.set("type", "");
+    } else {
+    }
+    return this.http.get<Search[]>(this.searchURL, { params: httpParams });
   }
 
   //get the current season of animes
