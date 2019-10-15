90a36d2b649a0122a4fc12727d4c098ababa5b6f
Bosi <dexing30100@gmail.com>
2019-10-15 18:33:29 +1100

    added bigquery to firebase functions

--- ani/functions/src/index.ts
+++ ani/functions/src/index.ts
@@ -1,8 +1,28 @@
 import * as functions from "firebase-functions";
+const BigQuery = require("@google-cloud/bigquery");
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
+});
+
+export const getReportData = functions.https.onRequest((req, res) => {
+  const bigquery = new BigQuery({
+    projectId: "anii-420"
+  });
+  const query = `SELECT * FROM \`anii-420.aniitest.testtest\``;
+  bigquery
+    .query({
+      query: query,
+      location: "australia-southeast1",
+      useLegacySql: false
+    })
+    .then(function() {
+      return res.status(200).send("hello");
+    })
+    .catch(function(error) {
+      return res.status(500).json(error);
+    });
 });
---
+++ ani/src/app/services/stat.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root"
+})
+export class StatService {
+  constructor(private http: HttpClient) {}
+  getData() {
+    return this.http
+      .get<any[]>(
+        "https://us-central1-anii-420.cloudfunctions.net/getReportData"
+      )
+      .pipe(map((result: any) => result));
+  }
+}
