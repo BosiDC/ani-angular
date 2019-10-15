bca09d30c7fcbe8704529a4a295353166a6dbed6
Bosi <dexing30100@gmail.com>
2019-10-15 21:46:43 +1100

    cors to firebase functions, more info for faves

--- ani/functions/src/index.ts
+++ ani/functions/src/index.ts
@@ -1,29 +1,34 @@
 import * as functions from "firebase-functions";
 const BigQuery = require("@google-cloud/bigquery");
+const cors = require("cors")({ origin: true });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+  cors(request, response, () => {
+    response.json("Hello from Firebase!");
+  });
 });
 
-export const getReportData = functions.https.onRequest((req, res) => {
-  const bigquery = new BigQuery({
-    projectId: "anii-420"
-  });
-  const query = `SELECT * FROM \`anii-420.aniitest.testtest\``;
-  bigquery
-    .query({
-      query: query,
-      location: "australia-southeast1",
-      useLegacySql: false
-    })
-    .then(function(results) {
-      const rows = results[0];
-      return res.status(200).json(rows);
-    })
-    .catch(function(error) {
-      return res.status(500).json(error);
+export const getReportData = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    const bigquery = new BigQuery({
+      projectId: "anii-420"
     });
+    const query = `SELECT * FROM \`anii-420.aniitest.testtest\``;
+    bigquery
+      .query({
+        query: query,
+        location: "australia-southeast1",
+        useLegacySql: false
+      })
+      .then(function(results) {
+        const rows = results[0];
+        return response.status(200).json(rows);
+      })
+      .catch(function(error) {
+        return response.status(500).json(error);
+      });
+  });
 });
