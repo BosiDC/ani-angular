6e45f0f69217eb518cd6b0f25eadd4766c990704
Bosi <dexing30100@gmail.com>
2019-10-16 20:33:12 +1100

    added http request for cloud functions

--- ani/src/app/services/stat.service.ts
+++ ani/src/app/services/stat.service.ts
@@ -15,9 +15,79 @@ export class StatService {
       .pipe(map((result: any) => result));
   }
 
-  getHello() {
-    return this.http.get<any[]>(
-      "https://us-central1-anii-420.cloudfunctions.net/helloWorld"
-    );
+  getTopTen() {
+    return this.http
+      .get<any[]>("https://us-central1-anii-420.cloudfunctions.net/getTopTen")
+      .pipe(map((result: any) => result));
+  }
+
+  getGenreFreq() {
+    return this.http
+      .get<any[]>(
+        "https://us-central1-anii-420.cloudfunctions.net/getGenreFreq"
+      )
+      .pipe(map((result: any) => result));
+  }
+
+  getGenreAvgScore() {
+    return this.http
+      .get<any[]>(
+        "https://us-central1-anii-420.cloudfunctions.net/getGenreAvgScore"
+      )
+      .pipe(map((result: any) => result));
+  }
+
+  getActionTopTen() {
+    return this.http
+      .get<any[]>(
+        "https://us-central1-anii-420.cloudfunctions.net/getActionTen"
+      )
+      .pipe(map((result: any) => result));
+  }
+
+  getAdventureTen() {
+    return this.http
+      .get<any[]>(
+        "https://us-central1-anii-420.cloudfunctions.net/getAdventureTen"
+      )
+      .pipe(map((result: any) => result));
+  }
+
+  getComedyTen() {
+    return this.http
+      .get<any[]>(
+        "https://us-central1-anii-420.cloudfunctions.net/getComedyTen"
+      )
+      .pipe(map((result: any) => result));
+  }
+
+  getDramaTen() {
+    return this.http
+      .get<any[]>("https://us-central1-anii-420.cloudfunctions.net/getDramaTen")
+      .pipe(map((result: any) => result));
+  }
+
+  getFantasyTen() {
+    return this.http
+      .get<any[]>(
+        "https://us-central1-anii-420.cloudfunctions.net/getFantasyTen"
+      )
+      .pipe(map((result: any) => result));
+  }
+
+  getRomanceTen() {
+    return this.http
+      .get<any[]>(
+        "https://us-central1-anii-420.cloudfunctions.net/getRomanceTen"
+      )
+      .pipe(map((result: any) => result));
+  }
+
+  getSportsTen() {
+    return this.http
+      .get<any[]>(
+        "https://us-central1-anii-420.cloudfunctions.net/getSportsTen"
+      )
+      .pipe(map((result: any) => result));
   }
 }
