b59f156b263d8a7e114332ba354e95826f6fe924
Bosi <dexing30100@gmail.com>
2019-10-17 17:11:45 +1100

    made changes to pie chart

--- ani/src/app/pages/stats/stats.component.ts
+++ ani/src/app/pages/stats/stats.component.ts
@@ -85,6 +85,29 @@ export class StatsComponent implements OnInit {
   public pieChartType: ChartType = "pie";
   public pieChartLabel: Label[] = this.genres;
   public pieChartData: number[] = this.freqGenres;
+  public pieChartLegend = true;
+  public pieChartColors = [
+    {
+      backgroundColor: [
+        "e6194b",
+        "#e6194b",
+        "#3cb44b",
+        "#ffe119",
+        "#4363d8",
+        "#f58231",
+        "#911eb4",
+        "#46f0f0",
+        "#f032e6",
+        "#bcf60c",
+        "#fabebe",
+        "#008080",
+        "#e6beff",
+        "#9a6324",
+        "#fffac8",
+        "#800000"
+      ]
+    }
+  ];
 
   constructor(private stat: StatService) {}
 
