cc73c9d7ecedea1efb834f0523e285b61d096267
Bosi <dexing30100@gmail.com>
2019-10-19 15:51:34 +1100

    change

--- ani/functions/src/index.ts
+++ ani/functions/src/index.ts
@@ -4,7 +4,7 @@ const cors = require("cors")({ origin: true });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
     response.json("Hello from Firebase!");
